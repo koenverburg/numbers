b1cc47b0dbb747fc4f58cd7fcf497b0bceeab2b4
Koen Verburg <creativekoen@gmail.com>
2020-05-17 23:22:28 +0200

    feat(login): wrote the few lines of the program

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import os
+from selenium import webdriver
+from dotenv import load_dotenv
+load_dotenv()
 
+browser = webdriver.Firefox()
+browser.get('https://github.com/login')
+
+if browser.find_element_by_id('login_field'):
+    loginElement = browser.find_element_by_id('login_field')
+    loginElement.send_keys(os.environ['GITHUB_USERNAME'])
+
+if browser.find_element_by_id('password'):
+    passwordElement = browser.find_element_by_id('password')
+    passwordElement.send_keys(os.environ['GITHUB_PASSWORD'])
+
+    form = browser.find_element_by_xpath('/html/body/div[3]/main/div/form')
+    form.submit()
